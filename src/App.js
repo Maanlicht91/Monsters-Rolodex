0a331f98b8918f33599fda41fbe3f35cb2a3c26c
Maanlicht91 <aynurdemir1991@hotmail.com>
2024-01-10 17:21:56 +0300

    Monsters-Rolodex

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"; //*These are Hooks
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  console.log("render");
+
+  const [searchField, setSearchField] = useState(""); //[value, setVAlue]
+  //console.log(searchField)
+  const [monsters, setMonsters] = useState([]);
+
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  const [title, setTitle] = useState("");
+
+  //!How to fetch api in functional component
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []); //* [] means dependincy so we leave it empty to not call the function again
+
+  useEffect(() => {
+    //* this method is a side effect method
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]); ///* Once monsters or searchfield are changes, then re-render the function
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
+  const onTitleChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setTitle(searchFieldString);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="app-title">{title}</h1>
+
+      <SearchBox
+        className="monster-search-box"
+        onChangeHandler={onSearchChange}
+        placeholder="search monsters"
+      />
+      <br />
+      <SearchBox
+        className="title-search-box"
+        onChangeHandler={onTitleChange}
+        placeholder="set title"
+      />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
-}
+};
 
 export default App;
